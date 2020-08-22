de9c782e79ae3baf741e3e9ec0c0fa84398490ec
MaxTitovitch <maxtitovitch@mail.ru>
2020-08-22 17:54:52 +0300

    CROS queries accepted

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ mongoClient.connect(function(err, client){
   });
 });
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 var CronJob = require('cron').CronJob;
 var job = new CronJob('0 */1 * * * *', function() {
   let notification_time = new Date().toLocaleTimeString().substr(0,5);
