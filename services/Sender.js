dfd2f14a9aa466b07b01862c2f0917f93a61d2a8
MaxTitovitch <maxtitovitch@mail.ru>
2020-08-23 18:56:06 +0300

    FCM added

--- services/Sender.js
+++ services/Sender.js
@@ -1,28 +1,27 @@
-// let FCM = require('fcm-node');
-// let serverKey = 'YOURSERVERKEYHERE'; // Put your server key here
-// let fcm = new FCM(serverKey);
+let FCM = require('fcm-node');
+//put your server key here
+let serverKey = 'AAAA0Lugnvw:APA91bHFrle_ozxFQT9S83tg3__4K90oAbdJyxFlhVa-PUiGK6ymhwk54014VGqJVRE83_HAtIw_KdbxIe6TiN1yJ-Gtif4HtEamegMhTl3X60BhJ4mXNtzKgipIE5zMO7OtfFCGg8M-';
+let fcm = new FCM(serverKey);
 
 module.exports = class Service {
   static send(deviceId, code) {
-    // let message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-    //   to: 'registration_token',
-    //   collapse_key: 'your_collapse_key',
-    //
-    //   notification: {
-    //     title: 'JSay registration code',
-    //     body: { message: code}
-    //   },
-    //
-    // };
-    //
-    // fcm.send(message, function(err, response){
-    //   if (err) {
-    //     console.log("Something has gone wrong!");
-    //   } else {
-    //     console.log("Successfully sent with response: ", response);
-    //   }
-    // });
-    console.log(deviceId, code)
+
+    let message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+      to: deviceId,
+
+      notification: {
+        title: 'JSay',
+        body: `Code: ${code}`
+      },
+    };
+
+    fcm.send(message, function (err, response) {
+      if (err) {
+        console.log("Something has gone wrong!");
+      } else {
+        console.log("Successfully sent with response: ", response);
+      }
+    });
   }
 
 };
