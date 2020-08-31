6c09504af24c7b9a01194958995dd68c1ebcdf3b
MaxTitovitch <maxtitovitch@mail.ru>
2020-08-31 16:29:35 +0300

    Grean SMS created

--- services/Sender.js
+++ services/Sender.js
@@ -1,32 +1,57 @@
 let FCM = require('fcm-node');
-
-let serverKey = 'AAAA0Lugnvw:APA91bHFrle_ozxFQT9S83tg3__4K90oAbdJyxFlhVa-PUiGK6ymhwk54014VGqJVRE83_HAtIw_KdbxIe6TiN1yJ-Gtif4HtEamegMhTl3X60BhJ4mXNtzKgipIE5zMO7OtfFCGg8M-';
+let serverKey = 'AAAA_zkXm70:APA91bEhCvRxVXWS7pfayvUALYZHL5JQ5Ir6y_REzeKmJaSdcUIv9sYbCA5Ajh2es07jmzFTDP2bOSEAmfM05jSRajoD_nICGpajbMN05M7NsHTey2ITqdobDRd60c9B8iO9Wgjn2Kt_';
 let fcm = new FCM(serverKey);
+
+const axios = require('axios').default;
+const userName = 'jsay';
+const password = 'jsay2020';
+
 
 
 module.exports = class Service {
-  static send(deviceId, code) {
+    static send(deviceId, code) {
 
-    let message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-      to: deviceId,
+        let message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+            to: deviceId,
 
-      notification: {
-        title: 'JSay',
-        body: `Code: ${code}`
-      },
-    };
+            notification: {
+                title: 'JSay',
+                body: `Code: ${code}`
+            },
+        };
 
-    fcm.send(message, function (err, response) {
-      if (err) {
-        console.log("Something has gone wrong!", err);
-      } else {
-        console.log("Successfully sent with response: ", response);
-      }
-    });
-  }
+        fcm.send(message, function (err, response) {
+            if (err) {
+                console.log("Something has gone wrong!", err);
+            } else {
+                console.log("Successfully sent with response: ", response);
+            }
+        });
+    }
 
-  static sendSMS (phone, code) {
-    console.log(phone, code);
-  }
+    static sendSMS(phone, code) {
+        let data = {
+            user: userName,
+            pass: password,
+            to: phone.substr(1),
+            txt: `Code: ${code}`
+        };
+        let query =  Object.keys(data).map(key => `${key}=${data[key]}`).join('&');;
+        let path = `https://api3.greensms.ru/sms/send?${query}`;
+        axios.get(path)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error.response.data);
+            });
+        // axios.get('http://api3.greensms.ru/sms/status?user=jsay&pass=jsay2020&id=bd414c1f-5c52-4aa2-aa22-119329e02110')
+        //     .then(function (response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error.response.data);
+        //     });
+    }
 
 };
